81a0503cbfd6991238610bca4b208a129be59249
hjq_glo <hjq_glo_123@123.com>
2019-10-22 23:39:19 +0800

    下载并使用 vue-router，同时建立 首页路由到 discovery 组件

---
+++ icoding-client/src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const Discovery = () => import("../view/discovery/discovery")
+
+const routes = [
+  { path: '/' ,redirect: '/discovery'},
+  { path: '/discovery' ,component: Discovery},
+
+]
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+
+export default router
