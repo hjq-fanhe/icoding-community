4e9a9590fab8de6915e68f5ec0cea339eacae825
hjq_glo <hjq_glo_123@123.com>
2019-10-22 17:00:28 +0800

    icoding 服务器端初始化

---
+++ icoding-server/src/main/java/com/fanhe/icoding/IcodingServerApplication.java
@@ -0,0 +1,13 @@
+package com.fanhe.icoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcodingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcodingServerApplication.class, args);
+    }
+
+}
